dd785165754c5c357a0f4eb9d4d5e30703b5c326
Pk060243 <131277796+Pk060243@users.noreply.github.com>
2026-03-16 16:02:06 +0700

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key="AIzaSyCPtLPqJ2o3mtcxcXeFiNsXYPbPCUz17dQ")
-
-print("รายชื่อโมเดลที่รองรับการทำ Embedding ใน Key ของคุณ:")
-for m in genai.list_models():
-    if 'embedContent' in m.supported_generation_methods:
-        print(f"-> {m.name}")
